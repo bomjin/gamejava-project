f4a0a67e0b967db42f0619cceeb7b5cf92214846
bomjin <bom96627@Naver.com>
2019-02-09 16:38:57 +0900

    Add Text File [GameJava2_03_Cast.java]

---
+++ step3/GameJava2_03_Cast.java
@@ -0,0 +1,19 @@
+package step3;
+
+public class GameJava2_03_Cast {
+
+	public static void main(String[] args) {
+		
+		int myInt;
+		float myFloat=(float) 3.0;
+		double myDouble;
+		
+		myInt=(int) myFloat;
+		myDouble= myInt;
+		System.out.println("myInt="+myInt+"\n"+"myFloat="+myFloat+"\n"+"myDouble="+myDouble);
+		
+		
+		
+	}
+
+}

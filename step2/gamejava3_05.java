b2af980dde975977f1f5fab4127fb05f14b24cae
bomjin <bom96627@Naver.com>
2019-02-09 16:22:39 +0900

    Add Text File [gamejava3_05]

---
+++ step2/gamejava3_05.java
@@ -0,0 +1,26 @@
+package gamejava3;
+
+public class gamejava3_05 {
+
+	public static void main(String[] args) {
+		
+		int num=2;
+		
+		switch(num) {
+		case 1:
+			System.out.println("결과는 1");
+			break;
+		case 2:
+			System.out.println("결과는 2");
+		case 3:
+			System.out.println("결과는 3");
+			break;
+			default:
+				System.out.println("결과는 1,2,3,이 아닙니다.");
+				break;
+			
+		}
+		
+	}
+
+}

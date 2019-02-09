5a1b0ba81b07bdea047ed227c71ad7b5249fa48f
bomjin <bom96627@Naver.com>
2019-02-09 16:27:28 +0900

    Add Text File [GameJava2_03_Argument.java]

---
+++ step3/GameJava2_03_Argument.java
@@ -0,0 +1,14 @@
+package step3;
+
+public class GameJava2_03_Argument {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i=0; i<args.length; i++) {
+		System.out.println(i+":"+args[i]);
+		
+		
+	}
+}
+}
